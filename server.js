a39f2dafcd5eea59a74d3752715ef77f7f26a685
kramduckner <buckner.a.mark@gmail.com>
2017-10-03 10:32:35 -0600

    added body-parser, .eslintrc, and the public directory

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+app.use(bodyParser.json({type:'application/json'}));
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 app.get("/", function(req, res) {
     res.sendfile('index.html');
